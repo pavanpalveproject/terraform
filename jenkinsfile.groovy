7d8cec163f2be3f37c6feed508ea10047422f5ac
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-02 16:15:45 +0530

    d

---
+++ jenkinsfile.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+
+    environment{
+        CLOUDSDK_CORE_PROJECT = 'o-media-2'
+    }
+
+    stages{
+
+
+        stage('gcp_authentication'){
+            steps{
+                sh 'gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS'
+            }
+        }
+        
+        stage('Checkout') {
+      steps {
+        git branch: 'master', url: 'https://github.com/pavanpalveproject/terraform'
+      }
+    }
+        
+        stage('Terraform Init') {
+            steps {
+              sh 'cd env/dev/'
+              sh 'terraform init'
+            }
+        }
+        stage('Terraform Plan') {
+            steps {
+                sh 'cd env/dev/'
+                sh 'terraform plan'
+                // sh 'terraform plan -var app_name=${TF_VAR_app_name} -var env=${TF_VAR_env} -out=tfplan'
+            }
+        }
+    }
+}
