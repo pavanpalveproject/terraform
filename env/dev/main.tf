64b92391ddae0018819768c5ce68d77f81bc21cb
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-02 20:20:17 +0530

    f

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     google = {
+#       source = "hashicorp/google"
+#     }
+#   }
+# }
 
-provider "google" {
-  credentials = file("/home/pavan_palve_project/o-media-2-59c02dc0d3cb.json")       
-  project     = "o-media-2"
-  region      = "asia-south1"
-}
+# provider "google" {
+#   credentials = file("/home/pavan_palve_project/o-media-2-59c02dc0d3cb.json")       
+#   project     = "o-media-2"
+#   region      = "asia-south1"
+# }
 
 module "compute_engine" {
   source                    = "../../modules"
