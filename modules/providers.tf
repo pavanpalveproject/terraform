b9a4b28239b6ed053b44a1de31244ceda15bc620
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-02 19:40:15 +0530

    vds

--- modules/providers.tf
+++ modules/providers.tf
@@ -1,6 +1,20 @@
 provider "google" {
-  
+  credentials = file("/home/pavan_palve_project/o-media-2-59c02dc0d3cb.json")  
    project = var.project_id
   region  = var.region
   zone    = var.zone
 }
+
+# terraform {
+#   required_providers {
+#     google = {
+#       source = "hashicorp/google"
+#     }
+#   }
+# }
+
+# provider "google" {
+       
+#   project     = "o-media-2"
+#   region      = "asia-south1"
+# }
