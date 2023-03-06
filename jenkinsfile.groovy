7f08442c462836200f0ed8baaa777b7b8c420139
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-06 17:35:52 +0530

    asda

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -21,52 +21,66 @@ pipeline {
     //     }
     // }
 
-    // stage('Terraform Init') {
-    //   steps {
-    //     script {
-    //       if (env.BRANCH_NAME.contains('dev')) {
-    //         dir('env/dev') {
-    //           sh "pwd"
-    //           sh 'terraform init'
-    //         }
-    //       }
-    //       if (env.BRANCH_NAME.contains("qa")) {
-    //         dir('env/qa') {
-    //           sh "pwd"
-    //           sh 'terraform init'
-    //         }
-    //       }
-    //       if (env.BRANCH_NAME.contains("prod")) {
-    //         dir('env/prod') {
-    //           sh "pwd"
-    //           sh 'terraform init'
-    //         }
-    //       }
-    //     }
-
-    //   }
-    // }
 
     stage('Terraform Init') {
-  steps {
-    script {
-      def envDir = ''
-      if (env.BRANCH_NAME.contains('dev')) {
-        envDir = 'dev'
-      } else if (env.BRANCH_NAME.contains('qa')) {
-        envDir = 'qa'
-      } else if (env.BRANCH_NAME.contains('prod')) {
-        envDir = 'prod'
-      }
-      if (envDir) {
-        dir("env/${envDir}") {
-          sh 'terraform init'
+        steps {
+            script {
+            def envDir = ''
+            if (env.BRANCH_NAME.contains('dev')) {
+                envDir = 'dev'
+            } else if (env.BRANCH_NAME.contains('qa')) {
+                envDir = 'qa'
+            } else if (env.BRANCH_NAME.contains('prod')) {
+                envDir = 'prod'
+            }
+            if (envDir) {
+                dir("env/${envDir}") {
+                sh 'terraform init'
+                }
+            }
+            }
         }
-      }
     }
-  }
-}
 
+    stage('Terraform fmt') {
+        steps {
+            script {
+            def envDir = ''
+            if (env.BRANCH_NAME.contains('dev')) {
+                envDir = 'dev'
+            } else if (env.BRANCH_NAME.contains('qa')) {
+                envDir = 'qa'
+            } else if (env.BRANCH_NAME.contains('prod')) {
+                envDir = 'prod'
+            }
+            if (envDir) {
+                dir("env/${envDir}") {
+                sh 'terraform fmt'
+                }
+            }
+            }
+        }
+    }
+
+    stage('Terraform validate') {
+        steps {
+            script {
+            def envDir = ''
+            if (env.BRANCH_NAME.contains('dev')) {
+                envDir = 'dev'
+            } else if (env.BRANCH_NAME.contains('qa')) {
+                envDir = 'qa'
+            } else if (env.BRANCH_NAME.contains('prod')) {
+                envDir = 'prod'
+            }
+            if (envDir) {
+                dir("env/${envDir}") {
+                sh 'terraform validate'
+                }
+            }
+            }
+        }
+    }
 
     stage('Terraform Plan') {
       steps {
@@ -109,58 +123,44 @@ pipeline {
       }
     }
 
-
     stage('Terraform Apply') {
-      steps {
-        script {
-          if (env.BRANCH_NAME.contains('dev')) {
-            dir('env/dev') {
-              sh "pwd"
-              // sh 'terraform apply -auto-approve terraform.plan'
+        steps {
+            script {
+            def envDir = ''
+            if (env.BRANCH_NAME.contains('dev')) {
+                envDir = 'dev'
+            } else if (env.BRANCH_NAME.contains('qa')) {
+                envDir = 'qa'
+            } else if (env.BRANCH_NAME.contains('prod')) {
+                envDir = 'prod'
             }
-          }
-          if (env.BRANCH_NAME.contains("qa")) {
-            dir('env/qa') {
-              sh "pwd"
-              // sh 'terraform apply -auto-approve terraform.plan'
+            if (envDir) {
+                dir("env/${envDir}") {
+                sh 'terraform apply -auto-approve terraform.plan'
+                }
             }
-          }
-          if (env.BRANCH_NAME.contains("prod")) {
-            dir('env/prod') {
-              sh "pwd"
-              // sh 'terraform apply -auto-approve terraform.plan'
             }
-          }
         }
-
-      }
     }
 
-    stage('Merge'){
-      steps{
-        script {
-          if (env.BRANCH_NAME.contains('dev')) {
-            sh 'git checkout master && git merge origin/${BRANCH_NAME} && git push'
-          }
-          if (env.BRANCH_NAME.contains("qa")) {
-            sh 'git checkout master && git merge ${BRANCH_NAME} && git push'
-          }
-          if (env.BRANCH_NAME.contains("prod")) {
-            sh 'git checkout master && git merge ${BRANCH_NAME} && git push'
-          }
+    stage('Merge to Master') {
+        steps {
+            script {
+            def envDir = ''
+            if (env.BRANCH_NAME.contains('dev')) {
+                envDir = 'dev'
+            } else if (env.BRANCH_NAME.contains('qa')) {
+                envDir = 'qa'
+            } else if (env.BRANCH_NAME.contains('prod')) {
+                envDir = 'prod'
+            }
+            if (envDir) {
+                dir("env/${envDir}") {
+                sh 'git checkout master && git merge origin/${BRANCH_NAME} && git push'
+                }
+            }
+            }
         }
-      }
     }
-    // stage('Terraform Plan') {
-    //     steps {
-
-    //         dir('env/${env_}') {
-    //         sh "pwd"
-    //         sh 'terraform plan'
-    //         sh 'terraform plan -var app_name=${TF_VAR_app_name} -var env=${TF_VAR_env} -out=tfplan'
-    //         }
-
-    //     }
-    // }
   }
 }
