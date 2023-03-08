823a9110c582fb05b5d565defcf62b033008e044
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-08 17:30:31 +0530

    sdaa

--- gcp-op-as-infra-saas/jenkinsfile.groovy
+++ gcp-op-as-infra-saas/jenkinsfile.groovy
@@ -1,8 +1,29 @@
 def parts = BRANCH_NAME.split('-')
 def grade = parts[0]
 def env = "as-" + parts[1]
-def res = parts[2]
+// def res = parts[2]
+// def res =         // manually provide resource
 def base_path = "gcp-op-as-infra-saas"
+
+
+parameters {
+
+        // booleanParam, choice, file, text, password, run, or string
+
+        // booleanParam(defaultValue: true, description: '', name: 'booleanExample')
+
+        // string(defaultValue: "TEST", description: 'What environment?', name: 'stringExample')
+
+        text(defaultValue: "vm", description: "give resource name", name: "res")
+
+        // choice(choices: 'US-EAST-1\nUS-WEST-2', description: 'What AWS region?', name: 'choiceExample')
+
+        // password(defaultValue: "Password", description: "Password Parameter", name: "passwordExample")
+
+    }
+
+
+
 
 pipeline {
   agent any
@@ -13,7 +34,7 @@ pipeline {
     stage('test-terraform-init'){
       steps{
         script{
-        dir("${base_path}/${env}/${res}") {
+        dir("${base_path}/${env}/$params.res") {
                 sh 'pwd'
                 sh 'ls'
                 sh 'terraform init'
