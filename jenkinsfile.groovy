e2e0a25517e4f78ab95bbf7f84f2f36db62ebb78
pavanpalveproject <125024203+pavanpalveproject@users.noreply.github.com>
2023-03-08 16:29:11 +0530

    asda

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,20 +2,21 @@ def parts = BRANCH_NAME.split('-')
 def grade = parts[0]
 def env = parts[1]
 def res = parts[2]
+def base_path = "gcp-op-as-infra-saas"
 
 pipeline {
   agent any
-
   environment {
     CLOUDSDK_CORE_PROJECT = 'o-media-2'
   }
   stages{
-    stage('test'){
+    stage('test-terraform-init'){
       steps{
         script{
-        dir("${grade}/${env}/${res}") {
+        dir("${base_path}/${grade}/${env}/${res}") {
                 sh 'pwd'
                 sh 'ls'
+                sh 'terraform init'
         }
       }
       }
